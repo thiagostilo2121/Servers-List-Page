94b13994ebf619b3bd3f9557121d2a755bdf70a5
Thiago Developer <robloxymasgame@gmail.com>
2021-06-23 20:41:41 -0300

    new

---
+++ port.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/lol', function(req, res) {
+  res.sendFile(path.join(__dirname, './server.html'));
+});
+
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
